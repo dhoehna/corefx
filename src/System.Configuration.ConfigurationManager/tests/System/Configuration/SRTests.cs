3f2cbd4f0e54fb357e65daac5b3883f82f44051f
dhoehna <dhoehna@yahoo.com>
2017-02-28 21:07:15 -0800

    Adding tests

---
+++ src/System.Configuration.ConfigurationManager/tests/System/Configuration/SRTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Resources;
+namespace System.Configuration
+{
+    public class SRTests
+    {
+        [Fact]
+        public void GetResourceString_ResourceKeyNull()
+        {
+            /*
+             *System.Resources.ResourceManager.GetString throws an ArgumentNullException if name is null.
+             * In this case, name is ResourceKey
+             */
+            Assert.Throws<ArgumentNullException>(() => SR.GetResourceString(null, ""));
+        }
+
+        [Fact]
+        public void GetResourceString_ResourceDoesNotExist()
+        {
+            Assert.Equal(null, SR.GetResourceString("Hello", "Hi"));
+        }
+
+        [Fact]
+        public void Format_NullArgs_NoPlaceholders()
+        {
+            Assert.Equal("Hello", SR.Format("Hello", null));
+        }
+
+        [Fact]
+        public void Format_NullArgsWithPlaceHolders()
+        {
+            Assert.Equal("Hello {0}", SR.Format("Hello {0}", null));
+        }
+
+        [Fact]
+        public void Format_NotNullArgs()
+        {
+            Assert.Equal("Hello, my name is Joe McCool McAwesome McSenior", SR.Format("Hello, my name is {0} {1} {2} {3}", "Joe", "McCool", "McAwesome", "McSenior"));
+        }
+
+        [Fact]
+        public void Format_OneObjectAsString()
+        {
+            Assert.Equal("Hello wonderful", SR.Format("Hello {0}", "wonderful"));
+        }
+
+        [Fact]
+        public void Format_OneObjectAsInt()
+        {
+            Assert.Equal("Hello 22", SR.Format("Hello {0}", 22));
+        }
+
+    }
+}
